0b6de183943073b04101286fdfd0fc7ce9e3c6f9
essellejaye <essellejayeis@gmail.com>
2022-07-28 20:41:49 -0700

    #46 Allow italic fonts using a common controls directive

--- src/apps/FFHacksterEx/StdAfx.cpp
+++ src/apps/FFHacksterEx/StdAfx.cpp
@@ -3,3 +3,5 @@
 //	stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
