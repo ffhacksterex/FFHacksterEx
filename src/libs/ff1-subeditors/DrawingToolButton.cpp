e912bbf210920853306676f855063b168e595b2a
essellejaye <essellejayeis@gmail.com>
2022-08-18 13:30:18 -0700

    #53 DrawingToolButton now derived from SimpleImageButton

--- src/libs/ff1-subeditors/DrawingToolButton.cpp
+++ src/libs/ff1-subeditors/DrawingToolButton.cpp
@@ -9,19 +9,12 @@
 
 // CDrawingToolButton
 
-IMPLEMENT_DYNAMIC(CDrawingToolButton, CButton)
+IMPLEMENT_DYNAMIC(CDrawingToolButton, CSimpleImageButton)
 
 CDrawingToolButton::CDrawingToolButton(UINT idimage, int index, COLORREF selcolor)
-	: m_index(index)
-	, SelColor((selcolor & 0x80000000) ? GetSysColor(COLOR_HIGHLIGHT) : selcolor)
+	: CSimpleImageButton(idimage, selcolor)
+	, m_index(index)
 {
-	m_restype = "PNG"; //TODO - for now, just PNG, add support for other types as needed.
-	m_idimage = idimage;
-	m_pgbm = Imaging::ImageLoad(AfxGetResourceHandle(), m_idimage, m_restype);
-	if (m_pgbm!= nullptr) {
-		m_hw = m_pgbm->GetWidth();
-		m_hh = m_pgbm->GetHeight();
-	}
 }
 
 CDrawingToolButton::~CDrawingToolButton()
@@ -35,7 +28,7 @@ int CDrawingToolButton::GetToolIndex() const
 
 sToolButtonData CDrawingToolButton::GetSpec() const
 {
-	return sToolButtonData{ m_idimage, "PNG", m_index };
+	return sToolButtonData{ m_idimage, m_restype, m_index };
 }
 
 void CDrawingToolButton::SuppressClickHandler(bool suppress)
@@ -44,43 +37,10 @@ void CDrawingToolButton::SuppressClickHandler(bool suppress)
 }
 
 
-BEGIN_MESSAGE_MAP(CDrawingToolButton, CButton)
+BEGIN_MESSAGE_MAP(CDrawingToolButton, CSimpleImageButton)
 	ON_CONTROL_REFLECT(BN_CLICKED, &CDrawingToolButton::OnBnClicked)
-	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
-
-void CDrawingToolButton::do_paint(CDC& dc)
-{
-	CRect client = Ui::GetClientRect(this);
-
-	// Background
-	auto pt = Ui::GetControlCursorPos(this);
-	bool hot = client.PtInRect(pt) == TRUE;
-	auto bkcolor = hot ? GetSysColor(COLOR_HOTLIGHT) : dc.GetBkColor();
-	dc.FillSolidRect(client, bkcolor);
-
-	// Image
-	//TODO - maintain aspect ratio when stretching
-	Gdiplus::Rect grcbmp{ 0, 0, m_hw, m_hh };
-	Gdiplus::Rect gclient{ client.left, client.top, client.Width(), client.Height() };
-	Gdiplus::Rect grc = gclient;
-	grc.Inflate(-2, -2);
-	grc.Intersect(gclient);
-	Gdiplus::Graphics g(dc.GetSafeHdc());
-	g.DrawImage(m_pgbm, grc, grcbmp.X, grcbmp.Y, grcbmp.Width, grcbmp.Height,
-		Gdiplus::Unit::UnitPixel);
-
-	// Border and focus rect
-	bool checked = GetCheck() == BST_CHECKED;
-	auto bdrcolor = checked ? GetSysColor(COLOR_HIGHLIGHT) : GetSysColor(COLOR_ACTIVECAPTION);
-	dc.Draw3dRect(client, bdrcolor, bdrcolor);
-
-	if (GetFocus() == this) {
-		client.InflateRect(-3, -3);
-		dc.DrawFocusRect(&client);
-	}
-}
 
 // CDrawingToolButton message handlers
 
@@ -93,12 +53,4 @@ void CDrawingToolButton::OnBnClicked()
 			parent->SendMessage(WMA_DRAWTOOLBNCLICK, 0, m_index);
 		}
 	}
-}
-
-void CDrawingToolButton::OnPaint()
-{
-	CPaintDC dc(this); // device context for painting
-					   // TODO: Add your message handler code here
-					   // Do not call CButton::OnPaint() for painting messages
-	do_paint(dc);
 }
--- src/libs/ff1-subeditors/DrawingToolButton.h
+++ src/libs/ff1-subeditors/DrawingToolButton.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SimpleImageButton.h>
+
 #define WMA_DRAWTOOLBNCLICK ((WM_APP) + 1)		// LPARAM = tool index
 
 typedef struct sToolButtonData {
@@ -10,7 +12,7 @@ typedef struct sToolButtonData {
 
 // CDrawingToolButton
 
-class CDrawingToolButton : public CButton
+class CDrawingToolButton : public CSimpleImageButton
 {
 	DECLARE_DYNAMIC(CDrawingToolButton)
 
@@ -19,23 +21,13 @@ public:
 	virtual ~CDrawingToolButton();
 
 	int GetToolIndex() const;
-	COLORREF SelColor;
 	sToolButtonData GetSpec() const;
 	void SuppressClickHandler(bool suppress);
 
 protected:
 	bool m_suppressclick = false;
-	UINT m_idimage;
-	CString m_restype;
 	int m_index = -1;
-	CBitmap m_image;
-	int m_hw = 0;
-	int m_hh = 0;
-	Gdiplus::Bitmap * m_pgbm;
-
-	void do_paint(CDC& dc);
 
 	DECLARE_MESSAGE_MAP()
 	afx_msg void OnBnClicked();
-	afx_msg void OnPaint();
 };
