d2bd8e2481c209c8d91681fe76afe1fc4bfa6a57
essellejaye <essellejayeis@gmail.com>
2023-02-07 17:36:43 -0700

    #155 Update year in version text

--- src/apps/FFHacksterEx/version-number.h
+++ src/apps/FFHacksterEx/version-number.h
@@ -6,6 +6,6 @@
 #define FFHEX_FILE_VERSION_TEXT "0,9,8,1"
 #define FFHEX_PROD_VERSION_NUMBER 0,9,8,1
 #define FFHEX_PROD_VERSION_TEXT "0,9,8,1"
-#define FFHEX_RELEASE_TEXT "October 2022"
-#define FFHEX_COPYRIGHT_TEXT "Copyright 2022 Esselle Jaye - MIT/Expat license"
+#define FFHEX_RELEASE_TEXT "February 2023"
+#define FFHEX_COPYRIGHT_TEXT "Copyright 2023 Esselle Jaye - MIT/Expat license"
 #define DD_COMPANY_TEXT  "DigitalDreamers.org - Esselle Jaye"
