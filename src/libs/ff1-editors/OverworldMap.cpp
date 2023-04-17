b9f4542559e76a4ff2b45a53dd1aeaff4cacb7cd
essellejaye <essellejayeis@gmail.com>
2023-04-16 17:39:16 -0700

    #159 Ui::MultiplyIf not takes the condition as the first param instead of hardcoding the check for VK_CONTROL

--- src/libs/ff1-editors/OverworldMap.cpp
+++ src/libs/ff1-editors/OverworldMap.cpp
@@ -1081,11 +1081,12 @@ void COverworldMap::handle_hscroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollB
 {
 	UNREFERENCED_PARAMETER(pScrollBar);
 
+	bool controlDown = IsKeyDown(VK_CONTROL);
 	switch (nSBCode) {
-	case SB_LINELEFT: ScrollOffset.x -= 1 * Ui::MultiplyIf(8); break;
-	case SB_LINERIGHT: ScrollOffset.x += 1 * Ui::MultiplyIf(8); break;
-	case SB_PAGELEFT: ScrollOffset.x -= 16 * Ui::MultiplyIf(2); break;
-	case SB_PAGERIGHT: ScrollOffset.x += 16 * Ui::MultiplyIf(2); break;
+	case SB_LINELEFT: ScrollOffset.x -= 1 * Ui::MultiplyIf(controlDown, 8); break;
+	case SB_LINERIGHT: ScrollOffset.x += 1 * Ui::MultiplyIf(controlDown, 8); break;
+	case SB_PAGELEFT: ScrollOffset.x -= 16 * Ui::MultiplyIf(controlDown, 2); break;
+	case SB_PAGERIGHT: ScrollOffset.x += 16 * Ui::MultiplyIf(controlDown, 2); break;
 	case SB_THUMBTRACK: ScrollOffset.x = nPos; break;
 	}
 	auto maxes = calc_scroll_maximums();
@@ -1107,11 +1108,12 @@ void COverworldMap::handle_vscroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollB
 {
 	UNREFERENCED_PARAMETER(pScrollBar);
 
+	bool controlDown = IsKeyDown(VK_CONTROL);
 	switch (nSBCode) {
-	case SB_LINEUP: ScrollOffset.y -= 1 * Ui::MultiplyIf(8); break;
-	case SB_LINEDOWN: ScrollOffset.y += 1 * Ui::MultiplyIf(8); break;
-	case SB_PAGEUP: ScrollOffset.y -= 16 * Ui::MultiplyIf(2); break;
-	case SB_PAGEDOWN: ScrollOffset.y += 16 * Ui::MultiplyIf(2); break;
+	case SB_LINEUP: ScrollOffset.y -= 1 * Ui::MultiplyIf(controlDown, 8); break;
+	case SB_LINEDOWN: ScrollOffset.y += 1 * Ui::MultiplyIf(controlDown, 8); break;
+	case SB_PAGEUP: ScrollOffset.y -= 16 * Ui::MultiplyIf(controlDown, 2); break;
+	case SB_PAGEDOWN: ScrollOffset.y += 16 * Ui::MultiplyIf(controlDown, 2); break;
 	case SB_THUMBTRACK: ScrollOffset.y = nPos; break;
 	}
 	auto maxes = calc_scroll_maximums();
